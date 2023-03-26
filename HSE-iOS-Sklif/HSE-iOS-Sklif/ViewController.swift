e893ad3ebbccc65249b7132b84edfc680784c3cd
Сергей Абросов <abrosovsergeya@gmail.com>
2023-03-26 15:06:01 +0300

    New branch develop

--- HSE-iOS-Sklif/HSE-iOS-Sklif/ViewController.swift
+++ HSE-iOS-Sklif/HSE-iOS-Sklif/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
 
