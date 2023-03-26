58da10c1fb335eff16480b66bb360c13b1af5c88
Сергей Абросов <abrosovsergeya@gmail.com>
2023-03-26 15:04:38 +0300

    Initial commit

---
+++ HSE-iOS-Sklif/HSE-iOS-Sklif/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
